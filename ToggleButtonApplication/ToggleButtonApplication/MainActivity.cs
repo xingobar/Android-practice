7fbe7a8b8f59edc2cb4b066dd7ed42d1255e4fc6
xingobar <gary840227@gmail.com>
2017-09-18 22:07:54 +0800

    toggle button example

---
+++ ToggleButtonApplication/ToggleButtonApplication/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ToggleButtonApplication
+{
+    [Activity(Label = "ToggleButtonApplication", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            ToggleButton toggleButton = FindViewById<ToggleButton>(Resource.Id.toggleButton);
+            toggleButton.Click += (o, s) => {
+                if(toggleButton.Checked)
+                {
+                    Toast.MakeText(this, "Checked", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Not Checked", ToastLength.Short).Show();
+                }
+            };
+        }
+    }
+}
+
