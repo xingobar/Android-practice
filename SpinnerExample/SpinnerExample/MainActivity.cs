6bdad3ca245c34e37cd7be98c80f0af5f4d10e17
xingobar <gary840227@gmail.com>
2017-09-23 17:10:55 +0800

    spinner example

---
+++ SpinnerExample/SpinnerExample/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SpinnerExample
+{
+    [Activity(Label = "SpinnerExample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            ArrayAdapter arrayAdapter = ArrayAdapter.CreateFromResource(this, Resource.Array.planets_array, Android.Resource.Layout.SimpleSpinnerItem);
+            arrayAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = arrayAdapter;
+            spinner.ItemSelected += new System.EventHandler<AdapterView.ItemSelectedEventArgs>(SpinnerItemSelected);
+        }
+
+        private void SpinnerItemSelected(object sender,AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = sender as Spinner;
+            string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
+            Toast.MakeText(this, toast, ToastLength.Short).Show();
+        }
+    }
+}
+
